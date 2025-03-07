0263c687b736a12560fb072f1dd18cae11173de1
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-03-08 00:16:13 +0800

    added connection to swiftui side, connecting data with binding to show scanned barcode and alerts for errors

--- Barcode_Scanner/Controllers/ScannerVC.swift
+++ Barcode_Scanner/Controllers/ScannerVC.swift
@@ -9,9 +9,9 @@ import Foundation
 import AVFoundation
 import UIKit
 
-enum CameraError: String {
-    case invalidDeviceInput = "Something wrong with camera. Unable to capture input."
-    case invalidScannedValue = "The value scanned is not valid. This app scans EAN-8 and EAN-13 barcodes."
+enum CameraError {
+    case invalidDeviceInput
+    case invalidScannedValue
 }
 
 protocol ScannerVCDelegate: AnyObject {
@@ -31,6 +31,22 @@ final class ScannerVC: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        guard let previewLayer = previewLayer else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        previewLayer.frame = view.layer.bounds
     }
     
     private func setupCaptureSession() {
@@ -92,6 +108,7 @@ extension ScannerVC: AVCaptureMetadataOutputObjectsDelegate {
             return
         }
         
+//        captureSession.stopRunning()
         scannerDelegate?.didFind(barcode: barcode)
     }
 }
--- Barcode_Scanner/Views/BarcodeScannerView.swift
+++ Barcode_Scanner/Views/BarcodeScannerView.swift
@@ -7,11 +7,36 @@
 
 import SwiftUI
 
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(
+        title: "Invalid Device Input",
+        message: "This device does not support barcode scanning.",
+        dismissButton: .default(Text("OK"))
+    )
+    
+    static let invalidScannedType = AlertItem(
+        title: "Invalid Scanned Type",
+        message: "The value scanned is not valid.",
+        dismissButton: .default(Text("OK"))
+    )
+}
+
 struct BarcodeScannerView: View {
+    @State private var scannedCode: String = ""
+    @State private var alertItem: AlertItem?
+    
     var body: some View {
         NavigationView {
             VStack {
-                Rectangle()
+                ScannerView(scannedCode: $scannedCode,
+                            alertItem: $alertItem)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 
                 Spacer()
@@ -20,13 +45,18 @@ struct BarcodeScannerView: View {
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
                     .font(.title)
                 
-                Text("No Bardcode Detected")
+                Text(scannedCode == "" ? "No Bardcode Detected" : scannedCode)
                     .bold()
                     .font(.largeTitle)
-                    .foregroundColor(.red)
+                    .foregroundColor(scannedCode == "" ? .red : .green)
                     .padding()
             }
             .navigationTitle("Barcode Scanner")
+            .alert(item: $alertItem) { alertItem in
+                Alert(title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
         }
     }
 }
