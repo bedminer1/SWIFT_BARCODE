cc1f06517138f653d364cd2047ffa66aee4f1d9c
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-03-07 16:55:54 +0800

    init

---
+++ Barcode_Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode_Scanner
+//
+//  Created by Alex on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
